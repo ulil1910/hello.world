56cff8edc11ed6d37033ddd588f33c22253bb48e
ulil1910 <meg.ulilalbab@gmail.com>
2016-06-24 07:56:38 +0700

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+nama : raw_input("Masukan nama anda disini : ")
+alamat : raw_input("Alamat rumah tinggal anda saat ini : ")
+hp : int(raw_input("No. Telp yang bisa dihubungi : ")
+
+Print "Nama Yang akan anda input adalah ", nama, "."
+Print "Tempat tinggal anda saat ini adalah: ",alamat, "."
+Print "Telpon yang bisa dihubungi adalah : ", hp, "."
